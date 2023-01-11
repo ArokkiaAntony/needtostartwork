96c1cc2c39a734354bb2abac2e82b52e9383e9a7
ArokkiaAntony <antonyvenkat96@gmail.com>
2023-01-11 09:48:31 +0530

    new pushing codes dailly root

---
+++ src/workprocessing/meetAfterpush.java
@@ -0,0 +1,24 @@
+package workprocessing;
+
+public class meetAfterpush {
+	public void Tc1() {
+		System.out.println("tony");
+
+	}
+
+	public void Tc2() {
+		System.out.println("tony");
+	}
+
+	public void Tc3() {
+		System.out.println("tony");
+	}
+
+	public void Tc4() {
+		System.out.println("tony");
+	}
+
+	public void Tc5() {
+		System.out.println("tony");
+	}
+}
